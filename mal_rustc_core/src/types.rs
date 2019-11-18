5bdd68ac900fcb3d628a72c078bd98b51ad921ca
emturner <em.turner@tutanota.com>
2019-11-18 00:19:09 +0000

    parsing strings

--- mal_rustc_core/src/types.rs
+++ mal_rustc_core/src/types.rs
@@ -5,7 +5,9 @@ pub enum MalAtom<'a> {
     Nil,
     Bool(bool),
     Special(&'a str),
-    String(&'a str),
+    // ideally this would be &'a str, but we need to escape the control characters
+    // so need to allocate a new string
+    String(String),
     Int(i32),
     List(LinkedList<MalAtom<'a>>),
 }
